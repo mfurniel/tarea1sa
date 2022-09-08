3b8c5ffb0d0b6fd3e53fb6f45c204a6a9de54d7b
Mauricio Furniel <mfurnielc@gmail.com>
2022-09-07 22:03:09 -0400

    ultima iteracion

--- trabajo1.cpp
+++ trabajo1.cpp
@@ -5,52 +5,80 @@
 #include <fstream>
 #include <map>
 #include <algorithm>
-
-ifstream leerArchivo(const string nombreArchivo){
-    ifstream archivo (nombreArchivo);
-    return ifstream;
-}
+#include <time.h>
 
 using namespace std;
-int main(int argc, const char* argv[]){
 
-    float treshold = argv[4];
-    cout << argv[2] << " " << treshold << endl;
-    // cin>>nombreArchivo;
+vector<string> w;
+int N, M, th;
 
-    
-    // string nombreArchivo = "prueba.txt";
-    // ifstream archivo(nombreArchivo);
-    ifstream archivo = leerArchivo(argv[2]);
-    
+ifstream leerArchivo(const string& nombreArchivo, const string& threshold)
+{
+    ifstream archivo(nombreArchivo);
+
     string linea;
 
-    int aux =0; 
-    vector <string> w;
-    while (getline(archivo, linea)){
-        w.push_back(linea); 
-    };
-    for (auto it=w.begin(); it!= w.end(); it++){
-        cout << *it << endl;
-    };
+    while (getline(archivo, linea))
+    {
+        w.push_back(linea);
+    }
 
-    int N=5;
-    int M=4;
-
-    int treshold2=treshold*N;
-    cout<<treshold2<<endl;
-
-    // for (int i = 0; i < 4; i++)
+    // for (auto it = w.begin(); it != w.end(); it++)
     // {
-    //     cout<<alfap['A']<<endl;
+    //     cout << *it << endl;
     // }
-      
+
+    auto it = nombreArchivo.begin();
+    int cont = 0; // contador de ' - '
+    string s;
+    while (*it != '.')
+    {
+        if (*it != '-')
+        {
+            s.push_back(*it);
+        }
+        else if (cont == 0)
+        {
+            N = stoi(s);
+            string new_m;
+            s = move(new_m);
+            cont++;
+        }
+        else
+        {
+            M = stoi(s);
+        }
+        it++;
+    }
+
+    th = stof(threshold) * M;
+
+    return archivo;
+}
+
+int main(int argc, const char* argv[]){
+
+    srand(time(NULL));
+
+    ifstream archivo;
+
+    if (argc == 5)
+    {
+        archivo = leerArchivo(argv[2], argv[4]);
+    }
+    else
+    {
+        archivo = leerArchivo("100-300-001.txt", "0.75");
+    }
+
+    cout << "terminé de leer" << endl;
+
     vector<char> wordfinal;
     int iteracion=0;
 
     do
     {
-        
+
         vector<pair<int,char>> alfap;
         alfap.push_back(pair<int,char>(0,'A'));
         alfap.push_back(pair<int,char>(0,'G'));
@@ -61,9 +89,10 @@ int main(int argc, const char* argv[]){
         alfap[2].first = 0;
         alfap[3].first = 0;
 
-       
+    //    cout<<"b"<<endl;
         for (int i = 0; i < N; i++)
         {
+            // cout << i << " " << N << endl;
             if (w[i][iteracion]=='A')
             {
                 alfap[0].first++;
@@ -81,36 +110,55 @@ int main(int argc, const char* argv[]){
                 alfap[3].first++;
             }
         }
-    
+
+        // cout<<"a"<<endl;
         sort(alfap.begin(), alfap.end());
 
-        wordfinal.push_back(alfap[0].second);
+            // for(size_t i = 1; i < alfap.size(); i++){
+
+            //     if(alfap[i-1].first == alfap[i].first){
+            //         wordfinal.push_back(alfap[rand()%i].second);
+            //     }
+            // }
+
+        // wordfinal.push_back(alfap[0].second);
 
         if (alfap[0].first==alfap[1].first && alfap[0].first==alfap[2].first && alfap[0].first==alfap[3].first) {
-            /*random */
+            wordfinal.push_back(alfap[rand()%4].second);
         }else if (alfap[0].first==alfap[1].first && alfap[0].first==alfap[2].first) {
-            /* random */
-        }else if (alfap[0].first==alfap[1].first && alfap[0].first==alfap[3].first) {
-            /* random */
-        }else if (alfap[0].first==alfap[2].first && alfap[0].first==alfap[3].first) {
-            /* random */
-        }else if (alfap[0].first==alfap[1].first)   {
-            /* random */
-        }else if(alfap[0].first==alfap[2].first)    {
-            /* random */
-        }else if (alfap[0].first==alfap[3].first)   {
-            /* random */
+            wordfinal.push_back(alfap[rand()%3].second);
+        }else if (alfap[0].first==alfap[1].first) {
+            wordfinal.push_back(alfap[rand()%2].second);
+        }else{
+            wordfinal.push_back(alfap[0].second);
         }
-        
-        
-        
-        
-        
-        
-        
+
+
+        // }else if (alfap[0].first==alfap[2].first && alfap[0].first==alfap[3].first) {
+        //     int aux;
+        //     do{
+        //        aux=rand()%4;
+        //     }while(aux==1);
+        //     wordfinal.push_back(alfap[aux].second);
+        // }else if (alfap[0].first==alfap[1].first)   {
+        //     wordfinal.push_back(alfap[rand()%2].second);
+        // }else if(alfap[0].first==alfap[2].first)    {
+        //     int aux;
+        //     do{
+        //        aux=rand()%3;
+        //     }while(aux==1);
+        //     wordfinal.push_back(alfap[aux].second);
+        // }else if (alfap[0].first==alfap[3].first)   {
+        //     int aux;
+        //     do{
+        //        aux=rand()%4;
+        //     }while(aux==1 || aux==2);
+        //     wordfinal.push_back(alfap[aux].second);
+        // }
+
         // wordfinal[iteracion]=alfap[0].second;
-        
-        
+
+
         // if (peso0==peso1)
         // {
         //     wordfinal[iteracion]=alfabeto[0];
@@ -124,34 +172,35 @@ int main(int argc, const char* argv[]){
         iteracion++;
 
     } while (iteracion < M);
-    
+
     for (int i = 0; i < M; i++)
     {
         cout<<wordfinal[i];
     }
     cout<<endl;
-    
+
     int p=0;
     int dif=0;
+
     for (int i = 0; i < N; i++)
     {
+        // cout<<w[i]<<" "<< th <<endl;
         dif=0;
         for (int j = 0; j < M; j++)
         {
-            
+
             if (wordfinal[j]!=w[i][j])
             {
                 dif++;
-            } 
+            }
         }
-        if (dif>=treshold2)
+        if (dif>=th)
         {
             p++;
         }
-         
+
     }
-    
-    cout<<dif<<endl;
+
+    cout<<p<<endl;
 
 }
-
